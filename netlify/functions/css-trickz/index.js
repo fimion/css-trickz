4d8dfd14f77c996b6a4e5ea58027a018c43a0398
Alex Riviere <fimion@users.noreply.github.com>
2021-05-27 14:51:07 +0000

    no require

--- netlify/functions/css-trickz/index.js
+++ netlify/functions/css-trickz/index.js
@@ -1,4 +1,26 @@
-const request = require('./httpPromise.js');
+const https = require('https');
+
+const request =  ((urlOptions, data="") => {
+    return new Promise((resolve, reject) => {
+      const req = https.request(urlOptions,
+        (res) => {
+          let body = '';
+          res.on('data', (chunk) => (body += chunk.toString()));
+          res.on('error', reject);
+          res.on('end', () => {
+            if (res.statusCode >= 200 && res.statusCode <= 299) {
+  
+              resolve({statusCode: res.statusCode, headers: res.headers, body: body});
+            } else {
+              reject('Request failed. status: ' + res.statusCode + ', body: ' + body);
+            }
+          });
+        });
+      req.on('error', reject);
+      req.write(data, 'binary');
+      req.end();
+    });
+  });
 
 function getAuthors (post){
     return post?._embedded?.author?.reduce((acc,auth)=>acc+`<span class="author">${auth.name}</span>`, '');
