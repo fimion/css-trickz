4d8cd132a7047ac8273fdb88c14f9aeaffba0380
Alex Riviere <fimion@users.noreply.github.com>
2022-09-03 21:56:07 +0000

    hardcode

--- netlify/functions/trigger-rebuild/index.js
+++ netlify/functions/trigger-rebuild/index.js
@@ -2,9 +2,7 @@ const {schedule} = require('@netlify/functions');
 const axios = require("axios");
 
 
-const crontab = process.env.CRONTAB_STRING || `0 */6 * * *`
-
-exports.handler = schedule(crontab, async function(event, context){
+exports.handler = schedule("0 */6 * * *", async function(event, context){
 
     await axios.post(process.env.REBUILD_TRIGGER_URL, {});
 
